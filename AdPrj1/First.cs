056a4d3598d6534a17d761ba349db6ce906f480d
Michal <mix@centrum.cz>
2017-03-24 10:40:26 +0100

    zero projects

---
+++ AdPrj1/First.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdPrj1
+{
+    public class First
+    {
+        public string Text { get { return "First"; } }
+    }
+}
---
+++ AdPrj2/Second.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdPrj2
+{
+    public class Second
+    {
+        public string Text { get { return "Second"; } }
+    }
+}
